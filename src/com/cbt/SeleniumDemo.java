0a7c378f70cc70e4fe463ebf80c7e15252baaa70
skyldrdm <email@email.com>
2018-06-09 17:13:41 -0400

    selenium project

---
+++ src/com/cbt/SeleniumDemo.java
@@ -0,0 +1,23 @@
+package com.cbt;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class SeleniumDemo {
+
+	public static void main(String[] args) {
+		
+	System.setProperty("webdriver.chrome.driver","/Users/kaan/Desktop/selenium dependencies/drivers/chromedriver");
+		
+	WebDriver driver=new ChromeDriver();
+		
+	driver.get("http://cybertekschool.com");
+	
+	//System.setProperty("webdriver.gecko.driver","/Users/kaan/Desktop/selenium dependencies/drivers/geckodriver");
+	
+	//WebDriver driver=new FirefoxDriver();
+	
+	//driver.get("http://cybertekschool.com");
+}
+}
